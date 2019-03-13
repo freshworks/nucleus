5345f1487a4c1984ddcbbfbdd3d267797343b91d
Rajasegar Chandran <rajasegar.chandiran@freshworks.com>
2019-03-13 17:45:52 +0530

    [TEST COMMIT] Introducing lint erros in templates and components

--- addon/components/nucleus-button.js
+++ addon/components/nucleus-button.js
@@ -1,7 +1,12 @@
 import Component from '@ember/component';
 import layout from '../templates/components/nucleus-button';
 
+const temp = "";
+
 export default Component.extend({
-  layout
+  layout,
+
+
+
   tagName: ''
 });
