d2d7beb70eb64237f54d37c5d16b6d4f3614f72a
Abhilash <abhilash.ramesh@freshworks.com>
2019-03-18 16:20:17 +0530

    [FEAT#9]: chore: Adds linters
    - [x] SCSS Lint
    - [x] Template lint
    - [x] ESLint upgrade

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,15 +4,15 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    // Add options here
+    stylelint: {
+      linterConfig:{
+        syntax: 'scss'
+      },
+      includePaths: [
+        'app/styles'
+      ]
+    }
   });
-
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
 
   return app.toTree();
 };
