c1f90335ac8fbe4999e9715aa1280f33ac4cacff
shibulijack <shibulijack@SHIBULIJACK-2.local>
2019-09-24 14:25:33 +0530

    Fixed lint

--- addon/components/nucleus-modal/footer.js
+++ addon/components/nucleus-modal/footer.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
-import { computed, get } from '@ember/object';
+import { computed } from '@ember/object';
 import layout from '../../templates/components/nucleus-modal/footer';
 
 /**
