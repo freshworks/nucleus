e8cfc683b347eeff22ae02960c92d800103aba91
shibulijack <shibulijack@SHIBULIJACK-2.local>
2019-09-25 12:36:18 +0530

    Reverting node-sass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,13 +1,9 @@
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
-var nodeSass = require('node-sass');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    sassOptions: {
-      implementation: nodeSass
-    },
     stylelint: {
       linterConfig:{
         syntax: 'scss'
