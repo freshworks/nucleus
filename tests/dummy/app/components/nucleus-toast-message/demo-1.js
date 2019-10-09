e7082116f741cdd177fc06468f5d0271f1a345a3
Shibu Lijack <shibulijack@gmail.com>
2019-10-09 21:05:46 +0530

    Added variants

--- tests/dummy/app/components/nucleus-toast-message/demo-1.js
+++ tests/dummy/app/components/nucleus-toast-message/demo-1.js
@@ -5,12 +5,38 @@ import { get } from '@ember/object'
 export default Component.extend({
   flashMessages: inject(),
   actions: {
-    foo() {
-      debugger;
+    success() {
       const flashMessages = get(this, 'flashMessages');
       flashMessages.success('Successfully saved!', {
         timeout: 2000,
+        sticky: false,
+        priority: 100,
+        showProgress: true
+      });
+    },
+    danger() {
+      const flashMessages = get(this, 'flashMessages');
+      flashMessages.danger('Successfully deleted!', {
+        timeout: 2000,
         sticky: true,
+        priority: 100,
+        showProgress: true
+      });
+    },
+    info() {
+      const flashMessages = get(this, 'flashMessages');
+      flashMessages.info('Some information', {
+        timeout: 2000,
+        sticky: false,
+        priority: 100,
+        showProgress: true
+      });
+    },
+    warning() {
+      const flashMessages = get(this, 'flashMessages');
+      flashMessages.warning('Some information', {
+        timeout: 2000,
+        sticky: false,
         priority: 100,
         showProgress: true
       });
