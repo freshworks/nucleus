1966f8bc46fef1f7df4af8b0045836be0d3b6402
Shibu Lijack <shibulijack@gmail.com>
2019-10-11 12:15:46 +0530

    Initial push

---
+++ addon/components/nucleus-inline-banner.js
@@ -0,0 +1,46 @@
+import { or } from '@ember/object/computed';
+import Component from '@ember/component';
+import { get, set } from '@ember/object';
+import { computed } from '@ember/object';
+import layout from '../templates/components/nucleus-inline-banner';
+
+const typeIconMap = {
+  'tip': 'bulb',
+  'info': 'info',
+  'warning': 'warning',
+  'error': 'error'
+};
+
+export default Component.extend({
+  layout,
+  classNames: ['quick-tip'],
+  classNameBindings: ['_typeClass'],
+  attributeBindings: ['data-test-id'],
+  'data-test-id': 'quick-tip',
+  type: 'tip', // ['tip', 'info', 'warning', 'error']
+  icon: 'Info',
+  size: 'large',
+  showCloseIcon: true,
+  isOpen: true,
+  isLinkAction: or('linkHref', 'linkAction'),
+  _typeClass: computed('type', 'isOpen', function() {
+    let type = get(this, 'type');
+    let isOpen = get(this, 'isOpen');
+    return (type && isOpen) ? `quick-tip--${type}` : null;
+  }),
+  typeIcon: computed('type', function() {
+    return typeIconMap[get(this, 'type')];
+  }),
+  actions: {
+    onCloseTip() {
+      if (get(this, 'onClose')) {
+        set(this, 'isOpen', false);
+        return get(this, 'onClose')();
+      }
+      set(this, 'isOpen', false);
+    },
+    onLinkAction() {
+      get(this, 'linkAction')();
+    }
+  }
+});
---
+++ tests/dummy/app/templates/docs/components/nucleus-inline-banner.md
@@ -0,0 +1,7 @@
+
+
+# Inline Banner
+
+{{nucleus-inline-banner/demo-1}}
+
+{{docs-note}}
