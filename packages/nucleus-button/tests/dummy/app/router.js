76774757a29ee931b6d499cc66eb51ede5438841
Shibu Lijack <shibulijack@gmail.com>
2019-10-22 14:35:55 +0530

    Initial push

---
+++ packages/nucleus-button/tests/dummy/app/router.js
@@ -0,0 +1,21 @@
+import AddonDocsRouter, { docsRoute } from 'ember-cli-addon-docs/router';
+import config from './config/environment';
+
+const Router = AddonDocsRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL,
+});
+
+Router.map(function() {
+  docsRoute(this, function() { 
+    this.route("usage");
+
+    this.route("components", function() {
+      this.route("nucleus-button");
+    });
+
+    this.route('not-found', { path: '/*path' });
+  });
+});
+
+export default Router;
