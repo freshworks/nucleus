603e8162c1888faea9f19a3a33d672027c84754b
shibulijack <shibulijack@SHIBULIJACK-2.local>
2019-10-25 17:01:04 +0530

    Fixed merge issues

---
+++ packages/inline-banner/index.js
@@ -0,0 +1,17 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = {
+  name: '@freshworks/inline-banner',
+
+  isDevelopingAddon() {
+    return true;
+  },
+
+  included(app, parentAddon) {
+    let target = (parentAddon || app);
+    target.options = target.options || {};
+    target.options.babel = target.options.babel || { includePolyfill: true };
+    return this._super.included.apply(this, arguments);
+  }
+};
