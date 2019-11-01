3a4dc4325eb79be7bb4b115d01e8b55f57537774
shibulijack <shibulijack@SHIBULIJACK-2.local>
2019-11-01 14:06:41 +0530

    Code cleanup

--- packages/@nucleus/ember-cli-build.js
+++ packages/@nucleus/ember-cli-build.js
@@ -4,13 +4,26 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 const { UnwatchedDir } = require('broccoli-source');
 const MergeTrees = require('broccoli-merge-trees');
 const Funnel = require('broccoli-funnel');
+const { readdirSync } = require('fs');
 
-function treeForPackage(packageName) {
-  let addonFiles = new Funnel(`../${packageName}/addon`, {
-    destDir: `@freshworks/${packageName}`
+const getDirectories = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dir => dir.isDirectory())
+    .map(dir => dir.name)
+
+// all packages except @nucleus
+const NUCLEUS_PACKAGES = getDirectories('../../packages').slice(1);
+
+const treesForPackages = () => {
+  let addonPackages = [];
+
+  NUCLEUS_PACKAGES.forEach((packageName) => {
+    addonPackages.push(new Funnel(`../${packageName}/addon`, {
+      destDir: `@freshworks/${packageName}`
+    }));
   });
 
-  return addonFiles;
+  return addonPackages;
 }
 
 module.exports = function(defaults) {
@@ -22,9 +35,7 @@ module.exports = function(defaults) {
   let tree = new MergeTrees(
     [
       baseFiles,
-      treeForPackage('button'),
-      treeForPackage('core'),
-      treeForPackage('modal')
+      ...treesForPackages()
     ]
   );
 
