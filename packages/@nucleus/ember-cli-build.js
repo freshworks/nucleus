946bd7ee3bebe54078d10835f2d9e7130a5148fc
shibulijack <shibulijack@SHIBULIJACK-2.local>
2019-11-01 12:48:39 +0530

    Fixes API doc generation problem

--- packages/@nucleus/ember-cli-build.js
+++ packages/@nucleus/ember-cli-build.js
@@ -1,8 +1,33 @@
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const { UnwatchedDir } = require('broccoli-source');
+const MergeTrees = require('broccoli-merge-trees');
+const Funnel = require('broccoli-funnel');
+
+function treeForPackage(packageName) {
+  let addonFiles = new Funnel(`../${packageName}/addon`, {
+    destDir: `@freshworks/${packageName}`
+  });
+
+  return addonFiles;
+}
 
 module.exports = function(defaults) {
+  // Build addon docs tree
+  let baseFiles = new Funnel(new UnwatchedDir('./'), {
+    include: ['package.json', 'README.md']
+  });
+
+  let tree = new MergeTrees(
+    [
+      baseFiles,
+      treeForPackage('button'),
+      treeForPackage('core'),
+      treeForPackage('modal')
+    ]
+  );
+
   let app = new EmberAddon(defaults, {
     stylelint: {
       linterConfig:{
@@ -11,6 +36,11 @@ module.exports = function(defaults) {
       includePaths: [
         'app/styles'
       ]
+    },
+    'ember-cli-addon-docs': {
+      projects: {
+        main: tree
+      }
     }
   });
 
