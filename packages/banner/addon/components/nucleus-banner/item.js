d80c1e2446c28498ca0046c24b691ceded7b7aab
shibulijack <shibulijack@SHIBULIJACK-2.local>
2019-11-04 11:57:46 +0530

    Added custom icons

--- packages/banner/addon/components/nucleus-banner/item.js
+++ packages/banner/addon/components/nucleus-banner/item.js
@@ -1,8 +1,20 @@
 import Component from '@ember/component';
 import { reads } from '@ember/object/computed';
 import layout from "../../templates/components/nucleus-banner/item";
+import { computed, get } from '@ember/object';
+
+const ICON_MAP = {
+  'success': 'circle-check',
+  'danger': 'circle-cross',
+  'warning': 'circle-info',
+  'info': 'circle-help'
+}
 
 export default Component.extend({
   layout,
-  bannerItem: reads('item')
+  bannerItem: reads('item'),
+  bannerIcon: computed('bannerItem.type', function() {
+    let type = get(this, 'bannerItem.type');
+    return (type && type in ICON_MAP) ? ICON_MAP[type] : null;
+  })
 });
