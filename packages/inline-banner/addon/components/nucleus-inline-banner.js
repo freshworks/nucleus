766159c544ec81d3880a7c2c5dbd2a3aacd0d725
Shibu Lijack <shibulijack@gmail.com>
2019-11-05 19:07:06 +0530

    Added API documentation comments

--- packages/inline-banner/addon/components/nucleus-inline-banner.js
+++ packages/inline-banner/addon/components/nucleus-inline-banner.js
@@ -3,7 +3,6 @@ import { get, set } from '@ember/object';
 import { computed } from '@ember/object';
 import { htmlSafe } from '@ember/template';
 import layout from '../templates/components/nucleus-inline-banner';
-
 const typeIconMap = {
   'info': 'circle-help',
   'success': 'circle-check',
@@ -11,34 +10,116 @@ const typeIconMap = {
   'danger': 'circle-cross'
 };
 
+/**
+  __Usage:__
+  [Refer component page](/docs/components/nucleus-inline-banner)
+
+  @class Nucleus Inline Banner
+  @namespace Components
+  @extends Ember.Component
+  @public
+*/
 export default Component.extend({
   layout,
   classNames: ['nucleus-inline-banner'],
-  classNameBindings: ['_typeClass', 'isOpen:show:hide'],
+  classNameBindings: ['_typeClass', '_isOpen:show:hide'],
   attributeBindings: ['data-test-id'],
+
+  /**
+  * data-test-id
+  *
+  * @field data-test-id
+  * @type string
+  * @private
+  */
   'data-test-id': 'nucleus-inline-banner',
-  type: 'info', // ['info', 'warning', 'success' ,'danger']
+
+  /**
+  * Type of the inline banner.
+  * [`info`, `success`, `warning`, `danger`]
+  *
+  * @field type
+  * @type string
+  * @public
+  * @default `info`
+  */
+  type: 'info',
+
+  /**
+  * Show or hide the close button
+  *
+  * @field isDismissible
+  * @type boolean
+  * @public
+  * @default true
+  */
   isDismissible: true,
-  isOpen: true,
+
+  /**
+  * Open/close state management
+  *
+  * @field _isOpen
+  * @type boolean
+  * @private
+  */
+  _isOpen: true,
+
+  /**
+  * Title of the banner. Can be plain text or HTML.
+  *
+  * @field title
+  * @type string
+  * @public
+  */
   title: null,
-  _title: computed('title', function() {
+
+  /**
+  * _title
+  *
+  * @computed _title
+  * @private
+  */
+  _title: computed('title', function () {
     return htmlSafe(get(this, 'title'));
   }),
-  _typeClass: computed('type', 'isOpen', function() {
+
+  /**
+  * _typeClass
+  *
+  * @computed _typeClass
+  * @private
+  */
+  _typeClass: computed('type', '_isOpen', function () {
     let type = get(this, 'type');
-    let isOpen = get(this, 'isOpen');
-    return (type && isOpen) ? `nucleus-inline-banner--${type}` : null;
+    let _isOpen = get(this, '_isOpen');
+    return type && _isOpen ? `nucleus-inline-banner--${type}` : null;
   }),
-  _icon: computed('type', function() {
+
+  /**
+  * _icon
+  *
+  * @computed _icon
+  * @private
+  */
+  _icon: computed('type', function () {
     return typeIconMap[get(this, 'type')];
   }),
   actions: {
+    /**
+    * The action that gets invoked on clicking the close button.
+    *
+    * @method onCloseTip
+    * @public
+    *
+    */
     onCloseTip() {
       if (get(this, 'onClose')) {
-        set(this, 'isOpen', false);
+        set(this, '_isOpen', false);
         return get(this, 'onClose')();
       }
-      set(this, 'isOpen', false);
+
+      set(this, '_isOpen', false);
     }
+
   }
 });
