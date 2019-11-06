9b1741c8383d6b4f96bb9bf48b841357cf64aa63
shibulijack <shibulijack@SHIBULIJACK-2.local>
2019-11-06 12:07:30 +0530

    Added API documentation

--- packages/banner/addon/services/nucleus-banner.js
+++ packages/banner/addon/services/nucleus-banner.js
@@ -1,25 +1,79 @@
 import { A } from '@ember/array';
 import Service from '@ember/service';
 import { set, get } from '@ember/object';
-import { DEFAULT_CONFIG } from '../constants/nucleus-banner'
+import { DEFAULT_CONFIG } from '../constants/nucleus-banner';
 
+/**
+  @class nucleus-banner
+  @namespace Service
+  @extends Ember.Service
+  @public
+*/
 export default Service.extend({
+  /**
+  * ``` javascript
+  * {
+  *   title: 'title',
+  *   type: 'success',
+  *   isDismissible: true,
+  *   content: {
+  *     linkAction: this.someAction.bind(this),
+  *     linkText: 'Click here'
+  *   },
+  *   componentName: 'some-component-name',
+  *   componentProps: {}
+  * }
+  * ```
+  *
+  * @field items
+  * @type null
+  * @public
+  */
   items: null,
 
+  /**
+  * init
+  *
+  * @method init
+  * @private
+  *
+  */
   init() {
     this._super(...arguments);
     set(this, 'items', A([]));
   },
 
+  /**
+  * Add an item with DEFAULT_CONFIG
+  *
+  * @method add
+  * @public
+  * @param {object} item
+  */
   add(item) {
     get(this, 'items').pushObject(Object.assign({}, DEFAULT_CONFIG, item));
   },
 
+  /**
+  * Remove an item
+  *
+  * @method remove
+  * @public
+  * @param {object} item
+  */
   remove(item) {
     get(this, 'items').removeObject(item);
   },
 
+  /**
+  * empty
+  *
+  * @method empty
+  * @public
+  *
+  */
   empty() {
     get(this, 'items').clear();
   }
+
 });
