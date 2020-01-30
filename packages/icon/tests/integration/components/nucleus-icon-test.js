dcda609b8ad72f6bbe310ad52fdcf262c5d80d5e
Sharath Sriram <sharath.sriram@freshworks.com>
2020-01-30 13:45:33 +0530

    Experiments VII

--- packages/icon/tests/integration/components/nucleus-icon-test.js
+++ packages/icon/tests/integration/components/nucleus-icon-test.js
@@ -4,7 +4,7 @@ import { render } from '@ember/test-helpers';
 import test from 'ember-sinon-qunit/test-support/test';
 import hbs from 'htmlbars-inline-precompile';
 import a11yAudit from 'ember-a11y-testing/test-support/audit';
-//import backstop from 'ember-backstop/test-support/backstop';
+import backstop from 'ember-backstop/test-support/backstop';
 
 module('Integration | Component | nucleus-icon', function(hooks) {
   setupRenderingTest(hooks);
@@ -33,9 +33,9 @@ module('Integration | Component | nucleus-icon', function(hooks) {
       assert.ok(true, 'no a11y errors found!');
     });
   });
-/*
+
   test('it passes visual regression tests', async function(assert) {
     await render(hbs`{{nucleus-icon name="nucleus-cross" size="mini"}}{{nucleus-icon name="nucleus-cross" size="small"}}{{nucleus-icon name="nucleus-cross" size="medium"}}{{nucleus-icon name="nucleus-cross" size="large"}}`);
     await backstop(assert, {scenario: {misMatchThreshold: 100}});
-  }); */
+  }); 
 });
