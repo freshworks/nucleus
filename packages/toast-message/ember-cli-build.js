702d4db090cf96a38e083e05a855d22950422c48
Prakash Chokalingam <prakash.chokalingam@freshworks.com>
2020-02-06 20:59:19 +0530

    no lint on tests

--- packages/toast-message/ember-cli-build.js
+++ packages/toast-message/ember-cli-build.js
@@ -5,15 +5,15 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    hinting: false,
-    stylelint: {
-      linterConfig:{
-        syntax: 'scss'
-      },
-      includePaths: [
-        'app/styles'
-      ]
-    }
+    hinting: false
+    // stylelint: {
+    //   linterConfig:{
+    //     syntax: 'scss'
+    //   },
+    //   includePaths: [
+    //     'app/styles'
+    //   ]
+    // }
   });
 
   return app.toTree();
