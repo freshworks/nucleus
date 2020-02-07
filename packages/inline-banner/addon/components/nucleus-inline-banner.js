d485ec4438b61d0e544aa9d919cbc88b0d233742
Shalini-Sivakumar <shalini.sivakumar@freshworks.com>
2020-02-07 10:54:38 +0530

    remove htmlSafe

--- packages/inline-banner/addon/components/nucleus-inline-banner.js
+++ packages/inline-banner/addon/components/nucleus-inline-banner.js
@@ -3,7 +3,6 @@ import { classNames, attributeBindings, classNameBindings, layout as templateLay
 import Component from '@ember/component';
 import { set, action } from '@ember/object';
 import { computed } from '@ember/object';
-import { htmlSafe } from '@ember/template';
 import layout from '../templates/components/nucleus-inline-banner';
 import { ICON_MAP } from '../constants/nucleus-inline-banner';
 
@@ -79,7 +78,7 @@ class NucleusInlineBanner extends Component {
   * @private
   */
   @computed('title', function () {
-    return htmlSafe(this.get('title'));
+    return this.get('title');
   })
   _title;
 
