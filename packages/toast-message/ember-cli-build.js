3c60d6f41c8a0041b69533ea2b0eeb7e1850f2e0
Prakash Chokalingam <prakash.chokalingam@freshworks.com>
2020-02-07 15:35:42 +0530

    lint-staged fix

--- packages/toast-message/ember-cli-build.js
+++ packages/toast-message/ember-cli-build.js
@@ -6,14 +6,6 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     hinting: false
-    // stylelint: {
-    //   linterConfig:{
-    //     syntax: 'scss'
-    //   },
-    //   includePaths: [
-    //     'app/styles'
-    //   ]
-    // }
   });
 
   return app.toTree();
