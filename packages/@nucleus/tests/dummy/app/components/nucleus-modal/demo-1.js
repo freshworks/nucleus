238fa5ea6608c246f7222f991b34edea6805ff34
Shibu Lijack <slijack@FDLMC944.local>
2020-02-20 16:39:01 +0530

    fix: documentation

--- packages/@nucleus/tests/dummy/app/components/nucleus-modal/demo-1.js
+++ packages/@nucleus/tests/dummy/app/components/nucleus-modal/demo-1.js
@@ -1,20 +1,25 @@
 // BEGIN-SNIPPET modal-component.js
 import Component from '@ember/component';
+import { inject } from '@ember/service';
 import RSVP from 'rsvp';
+import { get } from '@ember/object'
 
 export default Component.extend({
+  flashMessages: inject(),
   isModal1: false,
   actions: {
     onSubmit(value) {
+      get(this, 'flashMessages').success('Modal action complete.');
       return new RSVP.Promise((resolve) => {
         let wait = setTimeout(() => {
           clearTimeout(wait);
           resolve(value);
-        }, 1000)
+        }, 1000);
       });
     },
     onClose() {
-      alert('Modal closed');
+      // this.set('isModal1', false);
+      get(this, 'flashMessages').success('Modal closed.');
     }
   }
 });
--- packages/@nucleus/tests/dummy/app/components/nucleus-modal/demo-2.js
+++ packages/@nucleus/tests/dummy/app/components/nucleus-modal/demo-2.js
@@ -1,10 +1,13 @@
 // BEGIN-SNIPPET dialog-component.js
 import Component from '@ember/component';
+import { inject } from '@ember/service';
+import { get } from '@ember/object'
 
 export default Component.extend({
+  flashMessages: inject(),
   actions: {
     onSubmit(value) {
-      alert(value);
+      get(this, 'flashMessages').success(value);
     }
   }
 });
