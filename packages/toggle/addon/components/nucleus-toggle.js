483c994f28ed7377e82d36497ef51c4db30d95b3
ArockiaHelanFw <arockia.helanmary@freshworks.com>
2020-02-20 14:34:13 +0530

    Base toggle switch

--- packages/toggle/addon/components/nucleus-toggle.js
+++ packages/toggle/addon/components/nucleus-toggle.js
@@ -1,10 +1,16 @@
 import {
+  classNames,
+  attributeBindings,
+  classNameBindings,
   tagName,
   layout as templateLayout,
 } from '@ember-decorators/component';
-// import defaultProp from '@freshworks/core/utils/default-decorator';
+
+import defaultProp from '@freshworks/core/utils/default-decorator';
 
 import Component from '@ember/component';
+import { action, computed } from '@ember/object';
+import { alias } from '@ember/object/computed';
 import layout from "../templates/components/nucleus-toggle";
 
 /**
@@ -14,13 +20,32 @@ import layout from "../templates/components/nucleus-toggle";
 
   @class Nucleus Toggle
   @namespace Components
-  @extends Ember.Component
+  @extends Ember.Component 
   @public
 */
 @templateLayout(layout)
-@tagName('')
+@tagName('div')
+@classNames('nucleus-toggle')
+@classNameBindings(
+  '_sizeClass'
+)
+@attributeBindings('_disabled:disabled')
 class NucleusToggle extends Component {
+  @computed('size')
+  get _sizeClass() {
+    let size = this.get('size');
+    return size ? `nucleus-toggle--${size}` : 'nucleus-toggle--medium';
+  }
+  
+  @defaultProp
+  value = true;
+  @alias('value') checkedValue;
 
+  @action
+  onValueChange(value) {
+    this.set('checkedValue', value);
+  }
 }
 
 export default NucleusToggle;
+
