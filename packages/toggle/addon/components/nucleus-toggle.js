60e7947266b43bccb5621b67ac97f0b37bf20573
ArockiaHelanFw <arockia.helanmary@freshworks.com>
2020-03-09 14:16:10 +0530

    (feat)toggle: Added comments

--- packages/toggle/addon/components/nucleus-toggle.js
+++ packages/toggle/addon/components/nucleus-toggle.js
@@ -28,19 +28,50 @@ import layout from "../templates/components/nucleus-toggle";
 @attributeBindings('disabled:disabled', 'checkedValue:checked')
 
 class NucleusToggle extends Component {
+  /**
+  * Class name is added based on _sizeClass
+  *
+  * @computed _sizeClass
+  * @type string
+  * @private
+  */
   @computed('size')
   get _sizeClass() {
     let size = this.get('size');
     return size ? `nucleus-toggle--${size}` : 'nucleus-toggle--medium';
   }
 
+  /**
+  * Attribute name `checked` is added when set to true
+  *
+  * @field checkedValue
+  * @type boolean
+  * @default false
+  * @public
+  */
   @defaultProp
   value = false;
   @alias('value') checkedValue;
 
+  /**
+  * toggle sizes: `large` , `medium` & `small`
+  *
+  * @field size
+  * @type string
+  * @default null
+  * @public
+  */
   @defaultProp
   size = 'medium';
 
+  /**
+  * Flag to set disabled attribute
+  *
+  * @field disabled
+  * @type boolean
+  * @default false
+  * @public
+  */
   @defaultProp
   disabled = false;
 
