deb48de0b21c8c7ddbb36dba06684649d3554ca2
ArockiaHelanFw <arockia.helanmary@freshworks.com>
2020-03-09 16:41:12 +0530

    feat(toggle): Added a11y test

--- packages/toggle/tests/integration/components/nucleus-toggle-test.js
+++ packages/toggle/tests/integration/components/nucleus-toggle-test.js
@@ -4,6 +4,7 @@ import { render } from '@ember/test-helpers';
 import test from 'ember-sinon-qunit/test-support/test';
 import hbs from 'htmlbars-inline-precompile';
 import backstop from 'ember-backstop/test-support/backstop';
+import a11yAudit from 'ember-a11y-testing/test-support/audit';
 
 module('Integration | Component | nucleus-toggle', function(hooks) {
   setupRenderingTest(hooks);
@@ -83,5 +84,13 @@ module('Integration | Component | nucleus-toggle', function(hooks) {
       {{nucleus-toggle value=true}}
       {{nucleus-toggle value=false}}`);
     await backstop(assert,{scenario: {misMatchThreshold: 0.1}});
+  });
+
+  test('it passes a11y tests', async function(assert) {
+    await render(hbs`{{nucleus-toggle}}`);
+   
+    return a11yAudit(this.element).then(() => {
+      assert.ok(true, 'no a11y errors found!');
+    });
   });
 });
